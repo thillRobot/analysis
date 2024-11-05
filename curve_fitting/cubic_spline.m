90e51ce87c817e5c1b98097ec9a3b32a0482300c
thill <thill@tntech.edu>
2024-11-04 23:16:51 -0600

    added curve fitting/cubic_spline_example1

---
+++ curve_fitting/cubic_spline.m
@@ -0,0 +1,35 @@
+% cubic spline interpolation
+% ME3001, TNTech
+% Tristan Hill, November 4, 2024
+% method and notation is from Applied Numerical Methods with MATLAB,
+% Chapra and Canale 6th
+
+clear; clc; close all
+
+h1=1.5
+h2=2.5
+h3=2.0
+
+x1=3.0
+x2=4.5
+x3=7.0
+x4=9.0
+
+f1=2.5
+f2=1.0
+f3=2.5
+f4=0.5
+
+A=[1, 0, 0, 0
+   h1, 2*(h1+h2), h2, 0
+   0,  h2, 2*(h2+h3), h3
+   0,  0,         0,  1]
+b=[0 
+   3*(fdiff(f3,f2,x3,x2)-fdiff(f2,f1,x2,x1))
+   3*(fdiff(f4,f3,x4,x3)-fdiff(f3,f2,x3,x2))
+   0]
+c=inv(A)*b
+
+function df=fdiff(fi,fj,xi,xj)
+    df=(fi-fj)/(xi-xj);
+end
